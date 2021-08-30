8c9d102fd07d8365198fb79644428d851c0dd837
Jonilson Sousa <jonilsonds9@gmail.com>
2021-08-30 16:12:16 -0300

    Criando a janela sobre

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
 
 // Add live reload
 require('electron-reload')(__dirname, {
   electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
   hardResetMethod: 'exit'
-});
+})
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1000,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
 
-  mainWindow.loadFile(`${__dirname}/app/index.html`);
-};
+  // mainWindow.webContents.openDevTools()
+  mainWindow.loadFile(`${__dirname}/app/index.html`)
+}
 
 app.whenReady().then(() => {
-  createWindow();
-});
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-});
+  if (process.platform !== 'darwin') app.quit()
+})
 
+ipcMain.on('abrir_janela_sobre', () => {
+  let sobreWindow = new BrowserWindow({ 
+    width: 300,
+    height: 200
+  })
+
+  sobreWindow.loadFile(`${__dirname}/app/sobre.html`)
+})
